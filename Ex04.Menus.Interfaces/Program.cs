5a34549249387f1615ba23819997ff65ecacba1e
talkedmi <38419295+talkedmi@users.noreply.github.com>
2020-06-16 18:14:21 +0300

    Commit trial

---
+++ Ex04.Menus.Interfaces/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ex04.Menus.Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
